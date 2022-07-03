5b20bdbf9bcf9dafe2bb5ae3adaf2ae0b7ac6a08
Damirka1 <hamitov.damir3101@icloud.com>
2022-07-04 00:04:09 +0600

    Added nav bar and content block components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContentBlockComponent } from './content-block/content.block.component';
+import { NavBarComponent } from './nav-bar/nav.bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ContentBlockComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/content-block/content.block.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+
+@Component({
+    selector: 'content-block-component',
+    templateUrl: 'content.block.component.html',
+    styleUrls: ['content.block.component.scss']
+})
+export class ContentBlockComponent {
+
+}
