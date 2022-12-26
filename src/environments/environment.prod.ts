0e5ee421d95fe04c6a417dcc574d10c82560730e
Damirka1 <hamitov.damir3101@icloud.com>
2022-12-26 23:50:55 +0600

    Miss click

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,9 +7,9 @@ export const environment = {
   api_tracks_get: "/api/album/tracks/get/",
   api_track_get: "http://130.61.79.90:9000/tracks/",
 
-  api_image_get: "http://130.61.79.90:9000/image/",
-  api_image_medium_get: "http://130.61.79.90:9000/mediumImage/",
-  api_album_image_get: "http://130.61.79.90:9000/mediumImage/",
-  api_album_track_image_get: "http://130.61.79.90:9000/smallImage/",
-  api_album_track_metadata_image_get: "http://130.61.79.90:9000/image/"
+  api_image_get: "http://130.61.79.90:9000/images/",
+  api_image_medium_get: "http://130.61.79.90:9000/mediumImages/",
+  api_album_image_get: "http://130.61.79.90:9000/mediumImages/",
+  api_album_track_image_get: "http://130.61.79.90:9000/smallImages/",
+  api_album_track_metadata_image_get: "http://130.61.79.90:9000/images/"
 };
