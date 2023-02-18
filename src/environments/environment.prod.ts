a5758949fa1c4ec1656a5690691e767d73def203
Damirka1 <hamitov.damir3101@icloud.com>
2023-02-18 23:35:45 +0600

    Changed http -> https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
 
-  api_root: "http://damirka.space/api",
+  api_root: "https://damirka.space/api",
   api_main: "/main",
   api_album_get: "/album/get/",
   api_tracks_get: "/album/tracks/get/",
-  api_track_get: "http://damirka.space/fs/tracks/",
-  api_user_get: "http://damirka.space/api/user",
-  api_login: "http://damirka.space/api/login",
-  api_logout: "http://damirka.space/auth/logout",
+  api_track_get: "https://damirka.space/fs/tracks/",
+  api_user_get: "https://damirka.space/api/user",
+  api_login: "https://damirka.space/api/login",
+  api_logout: "https://damirka.space/auth/logout",
 };
